ac045de7e76e918da8a55e63589ff0c48c09c175
federicovarelamore <85002323+federicovarelamore@users.noreply.github.com>
2021-09-28 16:26:57 -0500

    Project Start
    HTML + CSS

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Variables
+const btnEnviar = document.querySelector('#enviar');
+
+eventListeners();
+function eventListeners (){
+    document.addEventListener('DOMContentLoaded', iniciarApp);
+
+}
+
+
+
+
+// Funciones
+function iniciarApp(){
+    btnEnviar.disabled = true;
+    btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
