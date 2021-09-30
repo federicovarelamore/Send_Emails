cc51cc8d1b17b23f6a4e2f090fc2200750fe9558
federicovarelamore <85002323+federicovarelamore@users.noreply.github.com>
2021-09-29 23:01:11 -0500

    NEW: function
    function to validate the form

--- js/app.js
+++ js/app.js
@@ -1,10 +1,22 @@
 // Variables
 const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+
+//Variables para campos del form
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
 
 eventListeners();
 function eventListeners (){
+    // Cuando la app arranca
     document.addEventListener('DOMContentLoaded', iniciarApp);
 
+    // Campos del formulario
+    email.addEventListener('blur', validarFormulario);
+    asunto.addEventListener('blur', validarFormulario);
+    mensaje.addEventListener('blur', validarFormulario);
 }
 
 
@@ -14,4 +26,28 @@ function eventListeners (){
 function iniciarApp(){
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed', 'opacity-50');
+}
+
+// Valida el formulario
+function validarFormulario(e){
+    if(e.target.value.lenght > 0 ){
+        
+    }else{
+        e.target.classList.add('border', 'border-red-500');
+    }
+
+    mostrarError();
+}
+
+function mostrarError(){
+    mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border', 'border-red-500', 'background-red-100', 'text-red-500', 'p-3', 'mt-5' , 'text-center', 'uppercase', 'error');
+
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+    }
+
+
 }
